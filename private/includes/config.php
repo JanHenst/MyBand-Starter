2260a30922112462f25b0b8eff7e0dac1aee3c44
JanHenst <42889823+JanHenst@users.noreply.github.com>
2019-06-21 08:37:35 +0200

    Add files via upload

---
+++ private/includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//if you're reading this that will mean you gay. except me(jan)
+
+$config = [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'u50678p47929_myband',
+    'DB_USER' => 'u50678p47929_jan',
+    'DB_PASSWORD' => 'janjan2002',
+    'ROOT' => dirname(dirname(__DIR__)),
+    'PRIVATE' => dirname(__DIR__),
+    'WEBROOT' => dirname(dirname(__DIR__)) . '/public'
+];
+
+return $config;
--- private/includes/functions.php
+++ private/includes/functions.php
@@ -1,12 +1,17 @@
 <?php
 
-function dbConnect($config)
+function dbConnect()
 {
+
+    $config = require __DIR__ . '/config.php';
 
     try {
         $dsn = 'mysql:host=' . $config['DB_HOST'] . ';dbname=' . $config['DB_NAME'];
 
         $connection = new PDO($dsn, $config['DB_USER'], $config['DB_PASSWORD']);
+
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
         return $connection;
 
@@ -14,4 +19,27 @@ function dbConnect($config)
         echo 'Fout bij maken van database verbinding: ' . $e->getMessage();
     }
 
+}
+
+function scripts(){
+  $con = dbConnect();
+  $sql = "SELECT * FROM home";
+  $statement = $con->query($sql);
+
+  $Description =  0;
+  $divText = 0;
+
+  for ($i=0; $i < 4; $i++) {
+    $Description++;
+    $divText++;
+    echo "<script>function Description$Description() {
+       var x = this.document.getElementById('homeText$divText');
+       if (x.style.visibility === 'hidden') {
+         x.style.visibility = '';
+       } else {
+         x.style.visibility = 'hidden';
+       }
+     }
+       </script>";
+  }
 }
